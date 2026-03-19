af689d3b38b41eddfefea756e8e884efab5037e0
Avikahlon <kahlon_abhijeet@icloud.com>
2026-03-19 17:26:48 +0800

    added player dimension table

--- dbt/models/dims/dim_player.sql
+++ dbt/models/dims/dim_player.sql
@@ -1,7 +1,6 @@
 select
     {{ dbt_utils.generate_surrogate_key(['player_name']) }} as player_key,
     player_name,
-    country,
-    link
+    country
 from {{ ref('stg_players') }}
 qualify row_number() over (partition by player_name order by games desc) = 1
